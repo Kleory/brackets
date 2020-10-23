7505a090339e9c6b106545711f2ced40289bd0d6
Kleory <okleory@gmail.com>
2020-10-23 20:59:46 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let config = '';
+  let stack = [];
+
+  bracketsConfig.map(index => {
+    config += index.join('');    
+  })
+  
+  for (let bracket of str) {
+    let configIndex = config.indexOf(bracket);    
+    if (configIndex == -1) {continue};
+
+    if (bracket === config[configIndex+1]){
+      stack[stack.length-1] == configIndex ? stack.pop() : stack.push(configIndex)
+    }else if (configIndex % 2 == 0) {
+      stack.push(configIndex + 1)
+    } else{
+      if (stack.pop() !== configIndex){
+        return false
+      }
+    }
+  }
+  return stack.length == 0;
 }
